8340523c8db3faaf7db12635936661ec4636b96d
Asus <Asus@192.168.1.4>
2020-11-15 18:06:28 +0330

    commented redundant lines

--- src/main/java/com/twelve/Main.java
+++ src/main/java/com/twelve/Main.java
@@ -6,11 +6,11 @@ public class Main {
 	
     public static void main(String[] args) {
     	LexicalAnalyzer analyzer = new LexicalAnalyzer();
-        System.out.println("enter a string");
-        Scanner s = new Scanner(System.in);
-        
-        StringBuilder str = new StringBuilder();
-        str.append(s.nextLine());
-        analyzer.scan(str);
+//        System.out.println("enter a string");
+//        Scanner s = new Scanner(System.in);
+//        
+//        StringBuilder str = new StringBuilder();
+//        str.append(s.nextLine());
+//        analyzer.scan(str);
     }
 }
