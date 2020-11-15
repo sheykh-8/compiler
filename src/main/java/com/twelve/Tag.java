a1f6c1537ce3f0439d52a527baede2e03b4649c6
Asus <Asus@192.168.1.4>
2020-11-15 18:01:43 +0330

    change terminal content

--- src/main/java/com/twelve/Tag.java
+++ src/main/java/com/twelve/Tag.java
@@ -2,5 +2,5 @@ package com.twelve;
 
 public class Tag {
 	public final static String NUM = "num", ID = "id", TRUE = "true", FALSE = "false", IF = "if", ELSE = "else",
-			BIG = "&B", SMALL = "&BM", EQUAL = "&MM", ASSIGN = "=", BIG_EQUAL = "&BM", SMALL_EQUAL = "&KM";
+			BIG = "big", SMALL = "small", EQUAL = "equal", ASSIGN = "assign", BIG_EQUAL = "bigEqual", SMALL_EQUAL = "smallEqual";
 }
