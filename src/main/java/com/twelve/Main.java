902bdea294ce89764e98dae8a15e9e4097e6f1dc
Hossein Lamei <HosseinLamei@outlook.com>
2020-11-15 23:10:58 +0330

    Update Main.java

--- src/main/java/com/twelve/Main.java
+++ src/main/java/com/twelve/Main.java
@@ -1,10 +1,12 @@
 package com.twelve;
 
+import java.util.ArrayList;
+
 public class Main {
 
 
     public static void main(String[] args) {
-        LexicalAnalyzer.scan("");
+        ArrayList<String> tokens = LexicalAnalyzer.scan("");
 
     }
 }
