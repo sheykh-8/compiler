350b345ab47c4c9461a833e4a1e5d31a15878438
Asus <Asus@192.168.1.4>
2020-11-15 15:33:07 +0330

    removing spaces

--- src/main/java/com/twelve/LexicalAnalyzer.java
+++ src/main/java/com/twelve/LexicalAnalyzer.java
@@ -1,5 +1,19 @@
 package com.twelve;
 
 public class LexicalAnalyzer {
-
+	char characterWatch;
+	
+	public void scan(StringBuilder str) {
+		int i = 0;
+		
+		while (i < str.length()) {
+			characterWatch = str.charAt(i);
+			if (characterWatch == ' ') {
+				str.deleteCharAt(i);
+			}
+			i++;
+		}
+		System.out.println(str);
+	}
+	
 }
--- src/main/java/com/twelve/Main.java
+++ src/main/java/com/twelve/Main.java
@@ -1,7 +1,16 @@
 package com.twelve;
 
+import java.util.Scanner;
+
 public class Main {
+	
     public static void main(String[] args) {
-        System.out.println("hi");
+    	LexicalAnalyzer analyzer = new LexicalAnalyzer();
+        System.out.println("enter a string");
+        Scanner s = new Scanner(System.in);
+        
+        StringBuilder str = new StringBuilder();
+        str.append(s.nextLine());
+        analyzer.scan(str);
     }
 }
