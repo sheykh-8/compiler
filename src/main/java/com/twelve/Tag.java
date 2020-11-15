c2d9b5ceda368c123cf8327fa333d26b65c0f1e5
Asus <Asus@192.168.1.4>
2020-11-15 17:26:57 +0330

    adjusted the terminals to sina# standards

--- src/main/java/com/twelve/Tag.java
+++ src/main/java/com/twelve/Tag.java
@@ -2,5 +2,5 @@ package com.twelve;
 
 public class Tag {
 	public final static String NUM = "num", ID = "id", TRUE = "true", FALSE = "false", IF = "if", ELSE = "else",
-			BIG = ">", SMALL = "<", EQUAL = "==", ASSIGN = "=", BIG_EQUAL = ">=", SMALL_EQUAL = "<=";
+			BIG = "&B", SMALL = "&BM", EQUAL = "&MM", ASSIGN = "=", BIG_EQUAL = "&BM", SMALL_EQUAL = "&KM";
 }
