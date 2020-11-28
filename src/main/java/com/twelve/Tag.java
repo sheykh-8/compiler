1d09725169c64288c2ef66cb1289c05ac81c8cbb
kamyar-nouri <kaminor24@gmail.com>
2020-11-28 15:13:40 +0330

    changed tags from string to int

--- src/main/java/com/twelve/Tag.java
+++ src/main/java/com/twelve/Tag.java
@@ -1,6 +1,6 @@
 package com.twelve;
 
 public class Tag {
-	public final static String NUM = "num", ID = "id", TRUE = "true", FALSE = "false", IF = "if", ELSE = "else",
-			BIG = "big", SMALL = "small", EQUAL = "equal", ASSIGN = "assign", BIG_EQUAL = "bigEqual", SMALL_EQUAL = "smallEqual";
+	public final static int NUM = 1000, ID = 1001, TRUE = 1002, FALSE = 1003, IF = 1004, ELSE = 1005,
+			BIG = 1006, SMALL = 1007, EQUAL = 1008, ASSIGN = 1009, BIG_EQUAL = 1010, SMALL_EQUAL = 1011;
 }
