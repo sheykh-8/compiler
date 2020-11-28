c293f2651ebfe763070a66d4a2f46bb7aac66879
kamyar-nouri <kaminor24@gmail.com>
2020-11-29 01:30:01 +0330

    added more comments

--- src/main/java/com/twelve/Token.java
+++ src/main/java/com/twelve/Token.java
@@ -5,11 +5,13 @@ public class Token {
 	public String scope; // Scope for characters
 	public int number; // Scope for numbers
 	
+	// Program uses this constructor if the token is characters
 	public Token(Integer tag, String scope) {
 		this.tag = tag;
 		this.scope = new String(scope);
 	}
 	
+	// Program uses this constructor if the token is number
 	public Token(Integer tag, int number) {
 		this.tag = tag;
 		this.number = number;
