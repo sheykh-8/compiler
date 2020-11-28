113499d9281eef7cb9ee2856d814cd0be795f6a8
kamyar-nouri <kaminor24@gmail.com>
2020-11-28 18:07:00 +0330

    variable type change and added while to tags

--- src/main/java/com/twelve/Tag.java
+++ src/main/java/com/twelve/Tag.java
@@ -1,6 +1,6 @@
 package com.twelve;
 
 public class Tag {
-	public final static int NUM = 1000, ID = 1001, TRUE = 1002, FALSE = 1003, IF = 1004, ELSE = 1005,
-			BIG = 1006, SMALL = 1007, EQUAL = 1008, ASSIGN = 1009, BIG_EQUAL = 1010, SMALL_EQUAL = 1011;
+	public final static Integer NUM = 1000, ID = 1001, TRUE = 1002, FALSE = 1003, IF = 1004, ELSE = 1005, WHILE = 1006,
+			BIG = 1007, SMALL = 1008, EQUAL = 1009, ASSIGN = 1010, BIG_EQUAL = 1011, SMALL_EQUAL = 1012;
 }
