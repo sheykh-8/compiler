2e31733bcce41a5e41db4c74b93bd2ef974d01d0
kamyar-nouri <kaminor24@gmail.com>
2020-11-29 00:38:01 +0330

    code cleanup

--- src/main/java/com/twelve/Word.java
+++ src/main/java/com/twelve/Word.java
@@ -3,10 +3,9 @@ package com.twelve;
 public class Word extends Token {
 
     public final String str;
-
+    
     public Word(Integer tag, String str) {
         super(tag);
         this.str = new String(str);
     }
-
 }
