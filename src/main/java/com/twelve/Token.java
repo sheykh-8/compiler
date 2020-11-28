ff3fa8ff83f0290e65db9e85d109fd1b94d198bb
kamyar-nouri <kaminor24@gmail.com>
2020-11-29 00:55:53 +0330

    merged classes Word & Number into one class

--- src/main/java/com/twelve/Token.java
+++ src/main/java/com/twelve/Token.java
@@ -1,8 +1,17 @@
 package com.twelve;
 
 public class Token {
-	public final int tag;
-	public Token(int terminal) {
-		tag = terminal;
+	public Integer tag;
+	public String scope;
+	public int number;
+	
+	public Token(Integer tag, String scope) {
+		this.tag = tag;
+		this.scope = new String(scope);
+	}
+	
+	public Token(Integer tag, int number) {
+		this.tag = tag;
+		this.number = number;
 	}
 }
