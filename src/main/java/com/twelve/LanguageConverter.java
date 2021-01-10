3c65890a25aca4706f2fba277d9b0e3fa3ea0294
kamyar-nouri <kaminor24@gmail.com>
2021-01-10 23:52:57 +0330

    added more to the sina# to c converter

--- src/main/java/com/twelve/LanguageConverter.java
+++ src/main/java/com/twelve/LanguageConverter.java
@@ -38,11 +38,30 @@ public class LanguageConverter {
 				index++;
 				break;
 			case Tag.IF:
-				input.append(" if");
+				input.append(" if"); // if
 				index++;
 				break;
 			case Tag.WHILE:
-				input.append(" while");
+				input.append(" while"); // while
+				index++;
+				break;
+/*
+ * the case block below needs some changes in order to recognize type specifiers and add &(address-of) operator after coma
+ */
+			case Tag.STRING:
+				input.append(table.get(index).getLexeme()); // string
+				index++;
+				break;
+			case Tag.ID:
+				input.append(table.get(index).getLexeme()); // id
+				index++;
+				break;
+			case Tag.NUM:
+				input.append(table.get(index).getLexeme()); // num
+				index++;
+				break;
+			case Tag.COMA:
+				input.append(" ,"); // ,
 				index++;
 				break;
 			case Tag.BIG:
@@ -82,7 +101,7 @@ public class LanguageConverter {
 				index++;
 				break;
 			case Tag.REMAIN:
-				input.append(" %");
+				input.append(" %"); // %
 				index++;
 				break;
 			case Tag.INC:
@@ -98,11 +117,31 @@ public class LanguageConverter {
 				index++;
 				break;
 			case Tag.OPEN_BRACES:
-				input.append(" [");
+				input.append(" {"); // {
 				index++;
 				break;
 			case Tag.CLOSE_BRACES:
-				input.append(" ]");
+				input.append(" }"); // }
+				index++;
+				break;
+			case Tag.OPEN_CURLY_BRACES:
+				input.append(" ("); // (
+				index++;
+				break;
+			case Tag.CLOSE_CURLY_BRACES:
+				input.append(" )"); // )
+				index++;
+				break;
+			case Tag.OPEN_PARANTHESES:
+				input.append(" ("); // (
+				index++;
+				break;
+			case Tag.CLOSE_PARANTHESES:
+				input.append(" )"); // )
+				index++;
+				break;
+			case Tag.TERMINATOR:
+				input.append(" ;"); // ;
 				index++;
 				break;
 			}
